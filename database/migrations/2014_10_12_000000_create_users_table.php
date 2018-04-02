9d44291646e9dce28751aee9734b803b49ce027c
givikoenig <givi.koenig@yandex.ru>
2018-04-02 17:26:09 +0200

    database_migration users table changed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('login',60);
         });
     }
 
